3277b9f4d47fa56c142392b1d65a4b2323cac018
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-14 15:59:17 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+set -eux
+
+BASE=$PWD
+SRC=$PWD/src
+OUT=$PWD/build
+ROOTFS=$PWD/rootfs
+
+mkdir -p $OUT $ROOTFS
+
+curl -sL https://github.com/gliderlabs/docker-alpine/blob/rootfs/library-edge/versions/library-edge/rootfs.tar.gz?raw=true | \
+    tar -xf -C $ROOTFS
+
+mv $ROOTFS/etc/localtime $ROOTFS/usr/share/zoneinfo
+ln -s /usr/share/zoneinfo $ROOTFS/etc/localtime 
+
+cat <<EOF > $OUT/etc/passwd
+root:x:0:0:root:/:/dev/null
+nobody:x:65534:65534:nogroup:/:/dev/null
+EOF
+
+cat <<EOF > $OUT/etc/group
+root:x:0:
+nogroup:x:65534:
+EOF
+
+cd $ROOTFS
+tar -cf $OUT/rootfs.tar .
+
+cat <<EOF > $OUT/Dockerfile
+FROM scratch
+
+ADD rootfs.tar /
+
+RUN apk add --no-cache nodejs
+
+ENV NODE_ENV=production
+
+EOF

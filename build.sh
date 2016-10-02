3dc9ea5b52a253ee6b894feb34c661643bb764e7
Markus Kobler <markus@distinctive.co>
2016-10-02 18:12:57 +0100

    Updating build

--- build.sh
+++ build.sh
@@ -1,38 +1,54 @@
-#!/usr/bin/env sh
-set -eux
+#!/usr/bin/env bash
+#
+# Download and build nodejs container
+set -euo pipefail
 
-SRC=$PWD/src
-OUT=$PWD/out
-ROOTFS=$PWD/rootfs
+src=${PWD}/src
+out=${PWD}/out
+rootfs=${PWD}/rootfs
 
-mkdir -p $OUT $ROOTFS
+_download() {
+  mkdir -p ${rootfs}
 
-curl -sL 'https://github.com/gliderlabs/docker-alpine/blob/rootfs/library-edge/versions/library-edge/rootfs.tar.gz?raw=true' | \
-    tar -C $ROOTFS -zxf -
+  curl -sL 'https://github.com/gliderlabs/docker-alpine/blob/rootfs/library-edge/versions/library-edge/rootfs.tar.gz?raw=true' | \
+      tar -C ${rootfs} -zxf -
+}
 
-mv $ROOTFS/etc/localtime $ROOTFS/usr/share/zoneinfo
-ln -s /usr/share/zoneinfo $ROOTFS/etc/localtime 
-
-cat <<EOF > $ROOTFS/etc/passwd
+_build() {
+  mv ${rootfs}/etc/localtime ${rootfs}/usr/share/zoneinfo
+  ln -s /usr/share/zoneinfo ${rootfs}/etc/localtime
+  
+  cat <<EOF > ${rootfs}/etc/passwd
 root:x:0:0:root:/:/dev/null
 nobody:x:65534:65534:nogroup:/:/dev/null
 EOF
 
-cat <<EOF > $ROOTFS/etc/group
+  cat <<EOF > ${rootfs}/etc/group
 root:x:0:
 nogroup:x:65534:
 EOF
 
-cd $ROOTFS
-tar -cf $OUT/rootfs.tar .
+  tar -cf ${out}/rootfs.tar -C ${rootfs} .
+}
 
-cat <<EOF > $OUT/Dockerfile
+# _dockerfile "version"
+_dockerfile() {
+  cat <<EOF > ${out}/version
+${1}
+EOF
+  
+  cat <<EOF > ${out}/Dockerfile
 FROM scratch
 
 ADD rootfs.tar /
 
-RUN apk add --no-cache nodejs
-
 ENV NODE_ENV=production
 
+RUN apk add --no-cache nodejs
+
 EOF
+}
+
+_download
+_build
+_dockerfile next
